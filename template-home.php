3917244deea3d18c2f902a7ca0294d8b7c0e8a26
Awesh Choudhary <aweshchoudhary7@gmail.com>
2024-07-23 23:14:56 +0530

    updated-header

--- template-home.php
+++ template-home.php
@@ -327,7 +327,7 @@ get_header();
             <h3>Becoming Who You Really Are: How to Grow Yourself and Your Organization</h3>
             <div class="md:w-4/5 w-full my-5">
                 <figure class="cursor-pointer relative group" onclick="lazyLoadVideo('89EXew_NZBM', this)">
-                    <img src="<?php echo get_template_directory_uri() ?>/assets/89EXew_NZBM-HD.jpg" class="image-cover" alt="">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/89EXew_NZBM-HD.jpg" class="image-cover" alt="">
                     <div class="play-btn group-hover:scale-110 transition-all">
                         <svg xmlns="http://www.w3.org/2000/svg" class="md:text-6xl sm:text-4xl text-3xl" width="1em" height="1em" viewBox="0 0 256 256">
                             <path fill="currentColor" d="M240 128a15.74 15.74 0 0 1-7.6 13.51L88.32 229.65a16 16 0 0 1-16.2.3A15.86 15.86 0 0 1 64 216.13V39.87a15.86 15.86 0 0 1 8.12-13.82a16 16 0 0 1 16.2.3l144.08 88.14A15.74 15.74 0 0 1 240 128" />
@@ -542,7 +542,7 @@ get_header();
             <div class="relative md:min-h-[600px] min-h-[400px]">
                 <div class="md:w-1/2 w-4/5 md:h-[80%] h-[60%] absolute bg-light-gray inset-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
                     <figure class="h-full border-2 border-secondary bg-white">
-                        <img src="<?php echo get_template_directory_uri() ?>/assets/certificate-ross.png" alt="" class="image-contain">
+                        <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/assets/certificate-ross.png" alt="" class="image-contain">
                     </figure>
 
                 </div>
