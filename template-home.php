0761805e0d4e11cd309516050571a4ae06f9b1ad
Awesh Choudhary <aweshchoudhary7@gmail.com>
2024-07-26 21:33:42 +0530

    updated

--- template-home.php
+++ template-home.php
@@ -69,13 +69,16 @@ get_header();
                             <?php endif; ?>
 
                             <?php if ($item["modal"]["modal_name"] && !empty($item["modal"]["modal_name"])) : ?>
-                                <button onclick="<?php echo str_replace(' ', '_', $item["modal"]["modal_name"]); ?>.showModal()"><?php echo $item["modal"]["modal_name"] ?></button>
+                                <button class="underline hover:text-third mt-1" onclick="<?php echo str_replace(' ', '_', $item["modal"]["modal_name"]); ?>.showModal()"><?php echo $item["modal"]["modal_name"] ?></button>
                                 <dialog id="<?php echo str_replace(' ', '_', $item["modal"]["modal_name"]); ?>" class="modal">
                                     <div class="modal-box rounded-none md:p-10 p-5 md:max-w-[60%]">
                                         <?php echo $item["modal"]["modal_content"]; ?>
-                                        <?php
-                                        $form_id = $item["modal"]["modal_id"];
-                                        echo do_shortcode("[wpforms id='$form_id']"); ?>
+                                        <?php if ($item["modal"]["modal_id"] && !empty($item["modal"]["modal_id"])) {
+
+                                            $form_id = $item["modal"]["modal_id"];
+                                            echo do_shortcode("[wpforms id='$form_id']");
+                                        } ?>
+
                                     </div>
                                     <form method="dialog" class="modal-backdrop">
                                         <button>Close</button>
